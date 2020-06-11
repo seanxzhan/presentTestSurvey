4c63b04c32cc6926c103247e0c40ab3ee6dde3cf
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-11 16:25:43 -0400

    Initial Commit

---
+++ queries.js
@@ -0,0 +1,8 @@
+import { Pool } from 'pg'
+const pool = new Pool({
+    user: 'onijnjombokzhh',
+    host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
+    database: 'd5vubft84tih4p',
+    password: '123456',
+    port: 5432
+})
