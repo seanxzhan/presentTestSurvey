6f22b1ede809b3605bb471873388a73e043b7a69
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-11 17:42:01 -0400

    Completed build. Buggy.

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+// to look for a GET request on root 
+app.get('/', (request, response) => {
+    // setting json
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+// gettig exported function(s) from queries.js
+const db = require('./queries')
+
+// settig http request method, endpoint URl, function:
+app.get('/feedback', db.getUsers)
+
+// listen on port
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
--- queries.js
+++ queries.js
@@ -1,8 +1,29 @@
-import { Pool } from 'pg'
+const Pool = require('pg').Pool
+// const pool = new Pool({
+//     user: 'onijnjombokzhh',
+//     host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
+//     database: 'd5vubft84tih4p',
+//     password: '123456',
+//     port: 5432
+// })
 const pool = new Pool({
-    user: 'onijnjombokzhh',
-    host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
-    database: 'd5vubft84tih4p',
-    password: '123456',
+    user: 'postgres',
+    host: 'localhost',
+    database: 'api',
+    password: 'password',
     port: 5432
 })
+
+// GET request 
+const getFeedback = (request, response) => {
+    pool.query('SELECT * FROM feedback', (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).json(results.rows)
+      })
+}
+
+module.exports = {
+    getFeedback,
+}
