ff04df4ef7be62ef15b18de46143efa3ed42ec3f
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-12 15:54:44 -0400

    Merged to testexpt database. API probably not going to wrok.

--- queries.js
+++ queries.js
@@ -1,6 +1,9 @@
-const Pool = require('pg').Pool
+const { Pool } = require('pg');
 
 var ENV = 'prod';
+const DATABASE_URL = 'postgres://onijnjombokzhh:b71661552e670fb920ae4126dc7f\
+798a3925675cb8fed5dd73a06df9e06d56d1@ec2-34-202-88-122.compute-1.amazonaws\
+.com:5432/d5vubft84tih4p'
 var pool;
 
 if (ENV == 'dev') {
@@ -12,12 +15,18 @@ if (ENV == 'dev') {
         port: 5432
     })
 } else {
+    // pool = new Pool({
+    //     user: 'onijnjombokzhh',
+    //     host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
+    //     database: 'd5vubft84tih4p',
+    //     password: 'b71661552e670fb920ae4126dc7f798a3925675cb8fed5dd73a06df9e06d56d1',
+    //     port: 5432
+    // })
     pool = new Pool({
-        user: 'onijnjombokzhh',
-        host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
-        database: 'd5vubft84tih4p',
-        password: 'b71661552e670fb920ae4126dc7f798a3925675cb8fed5dd73a06df9e06d56d1',
-        port: 5432
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     })
 }
 
@@ -26,7 +35,7 @@ if (ENV == 'dev') {
 // argument for query can be viewed as psql cli input
 // GET request 
 const getFeedback = (request, response) => {
-    pool.query('SELECT * FROM apitest', (error, results) => {
+    pool.query('SELECT * FROM feedback', (error, results) => {
         if (error) {
           throw error
         }
@@ -39,7 +48,7 @@ const postFeedback = (request, response) => {
     const { userID, sliderVal } = request.body
 
     pool.query(
-        'INSERT INTO apitest (userID, sliderVal) VALUES ($1, $2) RETURNING *', 
+        'INSERT INTO feedback (userID, sliderVal) VALUES ($1, $2) RETURNING *', 
     [userID, sliderVal], (error, results) => {
         if (error) {
             throw error
