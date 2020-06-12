bf3e0c703ca5ce107b2e4ea42eb519215e634add
seanzhan0319 <seanzhan0319@hotmail.com>
2020-06-12 11:28:48 -0400

    Added postgres port. Not working.

--- queries.js
+++ queries.js
@@ -1,19 +1,27 @@
 const Pool = require('pg').Pool
-// const pool = new Pool({
-//     user: 'onijnjombokzhh',
-//     host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
-//     database: 'd5vubft84tih4p',
-//     password: '123456',
-//     port: 5432
-// })
+
+var ENV = 'prod';
+var pool;
+
+if (ENV == 'dev') {
+    pool = new Pool({
+        user: 'test',
+        host: 'localhost',
+        database: 'api',
+        password: '123456',
+        port: 5432
+    })
+} else {
+    pool = new Pool({
+        user: 'onijnjombokzhh',
+        host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
+        database: 'd5vubft84tih4p',
+        password: 'b71661552e670fb920ae4126dc7f798a3925675cb8fed5dd73a06df9e06d56d1',
+        port: 5432
+    })
+}
+
 // note: user is test, database is api, table in database is apitest
-const pool = new Pool({
-    user: 'test',
-    host: 'localhost',
-    database: 'api',
-    password: '123456',
-    port: 5432
-})
 
 // argument for query can be viewed as psql cli input
 // GET request 
